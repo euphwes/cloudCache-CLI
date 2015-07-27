064b7a4b229bb65d8f6bd7ae38d996ad394c1f8c
Wes Evans <euphwes@gmail.com>
2015-07-27 13:44:57 -0600

    Logic addition to make sure the config file is always in the same location (next to cc_cli.py).

--- cc_cli.py
+++ cc_cli.py
@@ -1,16 +1,19 @@
 """ The cloudCache CLI application module. """
 
 import sys, json
-from os.path import exists
+from os.path import exists, dirname, realpath, join
 
 # -------------------------------------------------------------------------------------------------
 
-CONFIG_FILE = '.ccconfig'
+# Get the dir of this script, and append the config filename to that path, since we want the config
+# file to always be at the same location (next to the script), not in the working directory
+CONFIG_FILE = join(dirname(realpath(__file__)), '.ccconfig')
 
 # -------------------------------------------------------------------------------------------------
 
 def ensure_config():
-    """ Ensures a config file exists. Write default server location and port. """
+    """ Ensures a config file exists. Write default server location and port. Don't touch anything
+    if the config file already exists. """
 
     if not exists(CONFIG_FILE):
         with open(CONFIG_FILE, 'w') as config_file:
